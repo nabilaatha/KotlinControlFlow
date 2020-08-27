b539a193b61463ef8a5367bd60e1824ad9de00a4
nabilaatha <68956012+nabilaatha@users.noreply.github.com>
2020-08-27 15:27:53 +0700

    Add files via upload

---
+++ ExpressionsAndStatements.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val hitung = sum(1 , 1 * 4)
+    print(hitung)
+}
+fun sum(value1: Int, value2: Int) = value1 + value2
---
+++ WhileAndDoWhile.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var counter = 1
+    do{
+        println("Hello Nabila")
+        counter ++
+    }while (counter <= 7    )
+}
